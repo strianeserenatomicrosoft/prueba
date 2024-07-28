ed2a73e29926cf8edd63611c2bdcbef6544a12f5
renatostrianese <renatostrianese@gmail.com>
2024-07-27 22:32:51 -0600

    Inital Commit 2

--- obj/Debug/net8.0/Prueba.AssemblyInfo.cs
+++ obj/Debug/net8.0/Prueba.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Prueba")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ecf7a898dc36174462e7ed379faaf028fdd5e5fb")]
 [assembly: System.Reflection.AssemblyProductAttribute("Prueba")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Prueba")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
